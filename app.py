d3fe2cd537dd789d22f0982feecd7aef6c0c4f21
charchit <charchitchandra23@gmail.com>
2025-06-17 03:35:38 +0530

    Step4: Handled POST request for aircraft maintenance system

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,8 +6,20 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/add-aircraft')
+@app.route('/add-aircraft',methods=['GET','POST'])
 def add_aircraft():
+    if request.method == 'POST':
+        aircraft_id = request.form['aircraft_id']
+        model = request.form['model']
+        manufacturer = request.form['manufacturer']
+        year = request.form['year']
+        status = request.form['status']
+        print("Form Submitted")
+
+        # Just print for now (can later save to DB )
+        print(f"Received: {aircraft_id},{model},{manufacturer},{year},{status}")
+
+        return redirect(url_for('home'))
     return render_template('add_aircraft.html')
 
 
